4c61ec9a8c88838327b7ef47aaef2be37fa56531
enrico-dgr <enrico.digrazia98@gmail.com>
2023-05-07 11:19:46 +0200

    added build script

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,20 @@ export const parseUnknownError = (customMsg: string) => (e: unknown) => {
 
   return new Error(err.stack ?? customMsg)
 }
+
+export const logger = {
+  // default
+  log: (...args: any) => console.log(...args),
+  // red
+  error: (...args: any) => console.log('\x1b[31m%s\x1b[0m', ...args),
+  // green
+  success: (...args: any) => console.log('\x1b[32m%s\x1b[0m', ...args),
+  // yellow
+  warning: (...args: any) => console.log('\x1b[33m%s\x1b[0m', ...args),
+  // blue
+  debug: (...args: any) => console.log('\x1b[34m%s\x1b[0m', ...args),
+  // magenta
+  infoStrong: (...args: any) => console.log('\x1b[35m%s\x1b[0m', ...args),
+  // cyan
+  infoWeak: (...args: any) => console.log('\x1b[36m%s\x1b[0m', ...args),
+}
